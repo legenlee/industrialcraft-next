97617c773447a1094f8c314e10607daec690f822
LegenLee <lsko1332@naver.com>
2022-01-29 23:31:30 +0900

    refactor: Rename package name to "io.legenlee.industrialcraft"

---
+++ src/main/java/io/legenlee/industrialcraft/IndustrialCraft.java
@@ -0,0 +1,24 @@
+package io.legenlee.industrialcraft;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(IndustrialCraft.MOD_ID)
+public class IndustrialCraft {
+    public static final String MOD_ID = "icnext";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public IndustrialCraft() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // TODO: 초기화 소스 작성
+    }
+}
