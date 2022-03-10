0c65ed33a956939d2230b154a209b74eb9b23fe7
LegenLee <lsko1332@naver.com>
2022-03-10 19:02:47 +0900

    feat: Implement rubber log state

---
+++ src/main/java/industrialcraft/block/properties/ICBlockStateProperties.java
@@ -0,0 +1,8 @@
+package industrialcraft.block.properties;
+
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+
+public class ICBlockStateProperties {
+    public static final BooleanProperty HAS_SAP = BooleanProperty.create("has_sap");
+    public static final BooleanProperty CAN_MAKE_SAP = BooleanProperty.create("can_make_sap");
+}
