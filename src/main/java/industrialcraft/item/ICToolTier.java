2fe47e7badd3fb1f0e3ed8b2f43abcbc4ac1ef01
LegenLee <lsko1332@naver.com>
2022-03-17 16:47:40 +0900

    refactor: Move industrialcraft/item/tier/ICToolTier.java to industrialcraft/item/ICToolTier.java

--- src/main/java/industrialcraft/item/ICToolTier.java
+++ src/main/java/industrialcraft/item/ICToolTier.java
@@ -1,26 +1,26 @@
-package industrialcraft.item.tier;
+package industrialcraft.item;
 
 import java.util.List;
 
 import industrialcraft.IndustrialCraft;
 import industrialcraft.common.ICItems;
-import net.minecraft.tags.BlockTags;
+import industrialcraft.tags.ICBlockTags;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.common.TierSortingRegistry;
 
-public class ToolTier {
+public class ICToolTier {
     public static final Tier BRONZE = TierSortingRegistry.registerTier(
             new ForgeTier(2, 224, 6.0f, 2.5f, 12,
-                    BlockTags.create(IndustrialCraft.modPrefix("needs_bronze_tool")),
+                    ICBlockTags.NEEDS_BRONZE_TOOL,
                     () -> Ingredient.of(ICItems.BRONZE_INGOT.get())),
             IndustrialCraft.modPrefix("bronze"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
 
     public static final Tier STEEL = TierSortingRegistry.registerTier(
             new ForgeTier(3, 1804, 8.0f, 3.0f, 12,
-                    BlockTags.create(IndustrialCraft.modPrefix("needs_steel_tool")),
+                    ICBlockTags.NEEDS_STEEL_TOOL,
                     () -> Ingredient.of(ICItems.STEEL_INGOT.get())),
             IndustrialCraft.modPrefix("steel"), List.of(Tiers.DIAMOND), List.of(Tiers.NETHERITE));
 }
