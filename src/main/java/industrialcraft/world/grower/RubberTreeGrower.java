7d6045223997107e6ae2c5eab3ac7754921f3baf
LegenLee <lsko1332@naver.com>
2022-03-22 18:56:08 +0900

    fix: Override superset method in RubberTreeGrower

--- src/main/java/industrialcraft/world/grower/RubberTreeGrower.java
+++ src/main/java/industrialcraft/world/grower/RubberTreeGrower.java
@@ -2,12 +2,16 @@ package industrialcraft.world.grower;
 
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.TreeFeatures;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 
 public class RubberTreeGrower extends AbstractTreeGrower {
+    @Nullable
+    @Override
     protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(Random random, boolean beeHive) {
         return TreeFeatures.OAK;
     }
