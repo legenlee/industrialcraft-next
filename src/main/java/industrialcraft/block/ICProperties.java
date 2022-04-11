fe2adf984b7c135fa8d9782cfa2465b2c0e89d4f
LegenLee <lsko1332@naver.com>
2022-04-11 18:46:00 +0900

    refator: Rename property running to working

--- src/main/java/industrialcraft/block/ICProperties.java
+++ src/main/java/industrialcraft/block/ICProperties.java
@@ -5,5 +5,5 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 public class ICProperties {
     public static final BooleanProperty HAS_SAP = BooleanProperty.create("has_sap");
     public static final BooleanProperty CAN_CREATE_SAP = BooleanProperty.create("can_create_sap");
-    public static final BooleanProperty RUNNING = BooleanProperty.create("running");
+    public static final BooleanProperty WORKING = BooleanProperty.create("working");
 }
