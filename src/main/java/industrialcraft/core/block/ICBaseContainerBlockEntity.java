3318b5a788b097c58a75649c1943cc3182b6f6f8
LegenLee <lsko1332@naver.com>
2022-04-12 18:58:30 +0900

    feat: Implement own container block entity

---
+++ src/main/java/industrialcraft/core/block/ICBaseContainerBlockEntity.java
@@ -0,0 +1,76 @@
+package industrialcraft.core.block;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Container;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.Nameable;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+// TODO: Write class to extend this for implementing machines, tanks, etc...
+public abstract class ICBaseContainerBlockEntity extends BlockEntity implements Container, MenuProvider, Nameable {
+    private Component name;
+
+    protected ICBaseContainerBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        if (tag.contains("CustomName", 8)) {
+            this.name = Component.Serializer.fromJson(tag.getString("CustomName"));
+        }
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        if (this.name != null) {
+            tag.putString("CustomName", Component.Serializer.toJson(this.name));
+        }
+    }
+
+    public void setCustomName(Component component) {
+        this.name = component;
+    }
+
+    @Override
+    public Component getName() {
+        return this.name != null ? this.name : this.getDefaultName();
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return this.getName();
+    }
+
+    @Override
+    public Component getCustomName() {
+        return this.name;
+    }
+
+    protected abstract Component getDefaultName();
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int p_39954_, Inventory p_39955_, Player p_39956_) {
+        return null;
+    }
+
+    protected abstract AbstractContainerMenu createMenu(int what, Inventory inventory);
+
+    @Override
+    public void reviveCaps() {
+        // TODO Auto-generated method stub
+        super.reviveCaps();
+    }
+}
