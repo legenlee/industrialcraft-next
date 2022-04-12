012420579a32e8e655f6fa2ff7619e05b548994e
LegenLee <lsko1332@naver.com>
2022-04-12 18:30:36 +0900

    refactor: Update creative mode tab label

--- src/main/java/industrialcraft/core/inventory/ICCreativeModeTab.java
+++ src/main/java/industrialcraft/core/inventory/ICCreativeModeTab.java
@@ -8,10 +8,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ICCreativeModeTab {
-    public static final CreativeModeTab ICNEXT = createCreativeModeTab(IndustrialCraft.MOD_ID, ICItems.TIN_INGOT);
+    public static final CreativeModeTab ICNEXT = createCreativeModeTab("general", ICItems.TIN_INGOT);
 
     public static final CreativeModeTab createCreativeModeTab(String label, RegistryObject<Item> itemObject) {
-        return new CreativeModeTab(label) {
+        return new CreativeModeTab(IndustrialCraft.MOD_ID + "." + label) {
             @Override
             public ItemStack makeIcon() {
                 return new ItemStack(itemObject.get());
