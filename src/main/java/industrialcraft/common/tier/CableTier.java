f8067a1bca827dc1ecceaca0f8f8c2d99726482d
LegenLee <lsko1332@naver.com>
2022-04-27 19:07:59 +0900

    feat: Write basic placeholder referenced cable capacity

--- src/main/java/industrialcraft/common/tier/CableTier.java
+++ src/main/java/industrialcraft/common/tier/CableTier.java
@@ -1,11 +1,21 @@
 package industrialcraft.common.tier;
 
 public enum CableTier {
-    TIN,
-    COPPER,
-    GOLD,
-    IRON,
-    FIBRE;
+    TIN(32),
+    COPPER(128),
+    GOLD(512),
+    IRON(2048),
+    FIBRE(8192);
+
+    private final long maxCapacity;
+
+    CableTier(long maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
 
     private static final CableTier[] TIERS = values();
+
+    public long getMaxCapacity() {
+        return maxCapacity;
+    }
 }
