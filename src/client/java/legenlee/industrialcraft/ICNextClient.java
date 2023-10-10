54ce96f680fcbd28feb162f3cecb1a65860d6c5f
LegenLee <lsko1332@naver.com>
2023-10-10 16:58:10 +0900

    refactor: Rename template class name
    - Rename 'Example' to 'ICNext'
    - Rename mod id to 'icnext'
    - Rename mod name to 'IndustrialCraft-Next'
    - Update 'src/main/resources/assets/fabric.mod.json'
    - Update 'gradle.properties'

--- src/client/java/legenlee/industrialcraft/ICNextClient.java
+++ src/client/java/legenlee/industrialcraft/ICNextClient.java
@@ -2,7 +2,7 @@ package legenlee.industrialcraft;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class TemplateModClient implements ClientModInitializer {
+public class ICNextClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/legenlee/industrialcraft/ICNextDataGenerator.java
+++ src/main/java/legenlee/industrialcraft/ICNextDataGenerator.java
@@ -3,7 +3,7 @@ package legenlee.industrialcraft;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
+public class ICNextDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
